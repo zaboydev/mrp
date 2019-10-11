b0c15fdfb177f62985585b270fbe3e7d0fe1ad44
aidanurul99 <aidanurul99@rocketmail.com>
2019-10-11 09:29:09 +0700

    perbaikan print prl dari po

--- application/models/Purchase_Request_Model.php
+++ application/models/Purchase_Request_Model.php
@@ -2427,6 +2427,8 @@ class Purchase_Request_Model extends MY_Model
       $request['items'][$key]['ytd_budget'] = $row['ytd_budget'];
       $request['items'][$key]['ytd_used_quantity'] = $row['ytd_used_quantity'];
       $request['items'][$key]['ytd_used_budget'] = $row['ytd_used_budget'];
+      $request['items'][$key]['on_hand_qty'] = $this->tb_on_hand_stock($value['id'])->sum;
+      $request['items'][$key]['info_on_hand_qty'] = $this->info_on_hand($value['id']);
     }
 
 
