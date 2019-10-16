e8a019337b7529375d7555c0664ed6d73e39e7a9
aidanurul99 <aidanurul99@rocketmail.com>
2019-10-16 09:22:29 +0700

    perbaikan status poe jika sdh po+report itemdetail

---
+++ application/config/modules/purchase_item_detail.php
@@ -0,0 +1,20 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+$config['module']['purchase_item_detail']['visible']        = FALSE;
+$config['module']['purchase_item_detail']['main_warehouse'] = TRUE;
+$config['module']['purchase_item_detail']['parent']         = 'report';
+$config['module']['purchase_item_detail']['label']          = 'Purchase Item Detail';
+$config['module']['purchase_item_detail']['name']           = 'Purchase Item Detail';
+$config['module']['purchase_item_detail']['route']          = 'purchase_item_detail';
+$config['module']['purchase_item_detail']['view']           = config_item('module_path') . 'purchase_item_detail/';
+$config['module']['purchase_item_detail']['language']       = 'prl_poe_lang';
+$config['module']['purchase_item_detail']['helper']         = 'material_slip_helper';
+$config['module']['purchase_item_detail']['table']          = 'tb_purchase_orders';
+$config['module']['purchase_item_detail']['model']          = 'Purchase_Item_Detail_Model';
+$config['module']['purchase_item_detail']['permission']     = array(
+  'index'     => 'PROCUREMENT,PIC PROCUREMENT,SUPER ADMIN,FINANCE',
+  'info'      => 'PROCUREMENT,PIC PROCUREMENT,SUPER ADMIN,FINANCE',
+  'print'     => 'PROCUREMENT,PIC PROCUREMENT,SUPER ADMIN,FINANCE',
+  'document'  => 'PROCUREMENT,PIC PROCUREMENT,SUPER ADMIN,FINANCE',//tambhan supervisor
+  'payment'   => 'FINANCE,SUPER ADMIN',//tambhan supervisor
+);
