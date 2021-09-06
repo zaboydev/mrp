397d6887fd7c8d1fafddacb5dbe40e707d419db1
aidanurul99 <aidanurul99@rocketmail.com>
2021-09-07 00:12:01 +0800

    perbaikan edit poe inv pesawat

--- application/models/Purchase_Order_Evaluation_Model.php
+++ application/models/Purchase_Order_Evaluation_Model.php
@@ -496,6 +496,8 @@ class Purchase_Order_Evaluation_Model extends MY_Model
       $this->db->set('created_by', $created_by);
       if ($approval == 'without_approval') {
         $this->db->set('approved_by', $approval);
+      }else{
+        $this->db->set('approved_by', null);
       }
       $this->db->set('warehouse', $warehouse);
       $this->db->set('category', $category);
