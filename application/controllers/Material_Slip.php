7cbdcb5385fd797b5f5ae09267ca51bd98bf7831
aidanurul99 <aidanurul99@rocketmail.com>
2021-09-29 15:49:17 +0800

    validasi tanggal saat create material slip

--- application/controllers/Material_Slip.php
+++ application/controllers/Material_Slip.php
@@ -628,6 +628,14 @@ class Material_Slip extends MY_Controller
           }
         }
 
+        $today    = date('Y-m-d');
+        $date     = strtotime('-2 day',strtotime($today));
+        $min_date = date('Y-m-d',$date);
+
+        if($_SESSION['usage']['issued_date'] < $min_date){
+          $errors[] = 'Issued Date is too Old. Minimal Date is '.$date.' !';
+        }
+
         if (!empty($errors)){
           $data['success'] = FALSE;
           $data['message'] = implode('<br />', $errors);
