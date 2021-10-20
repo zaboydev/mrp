5ebd6fed3359cc08c098960091cbe98cca9e21c7
aidanurul99 <aidanurul99@rocketmail.com>
2021-10-21 01:05:54 +0800

    view list po menampilkan remaining request

--- themes/material/modules/payment/list_po.php
+++ themes/material/modules/payment/list_po.php
@@ -9,7 +9,7 @@ $no_item = 1; ?>
         <td>&nbsp;</td>
         <td><?= print_number($detail['grand_total'], 2) ?></td>
         <td><?= print_number($detail['payment'], 2) ?></td>
-        <td><input id="sis_<?= $no ?>" value="<?= $detail['remaining_payment'] ?>" type="hidden"><?= print_number($detail['remaining_payment'], 2) ?></td>
+        <td><input id="sis_<?= $no ?>" value="<?= $detail['emaining_payment_request'] ?>" type="hidden"><?= print_number($detail['emaining_payment_request'], 2) ?></td>
         <td><input id="in_<?= $no ?>" data-row="<?= $no ?>" type="number" class="sel_applied" value="0"></td>
         <td><button type="button" class="btn btn-xs btn-info btn-sm"><i class="fa fa-eye btn_view_detail" id="btn_<? $no ?>" data-row="<?= $no ?>" data-tipe="view"></i></button></td>
     </tr>
@@ -22,14 +22,14 @@ $no_item = 1; ?>
                 <td><?= print_number($detail_po['quantity_received'], 2) ?></td>
                 <td><?= print_number($detail_po['quantity_received'] * ($detail_po['unit_price'] + $detail_po['core_charge']), 2) ?></td>
                 <td><?= print_number($detail_po['total_amount'], 2) ?></td>
-                <td><?= print_number($detail_po['total_amount'] - $detail_po['left_paid_amount'], 2) ?></td>
-                <td><input id="sis_item_<?= $no ?>_<?= $no_item ?>" value="<?= $detail_po['left_paid_amount'] ?>" type="hidden"><?= print_number($detail_po['left_paid_amount'], 2) ?></td>
+                <td><?= print_number($detail_po['total_amount'] - $detail_po['left_paid_request'], 2) ?></td>
+                <td><input id="sis_item_<?= $no ?>_<?= $no_item ?>" value="<?= $detail_po['left_paid_request'] ?>" type="hidden"><?= print_number($detail_po['left_paid_request'], 2) ?></td>
                 <td><input id="in_item_<?= $no ?>_<?= $no_item ?>" data-parent="<?= $no ?>" data-row="<?= $no_item ?>" type="number" class="sel_applied_item sel_applied_<?= $no ?>" value="0"></td>
                 <td></td>
                 <?php $no_item++; ?>
             </tr>
         <?php endforeach; ?>
-        <?php if ($detail['additional_price_remaining'] != 0) : ?>
+        <?php if ($detail['additional_price_remaining_request'] != 0) : ?>
             <tr id="row_item_<?= $no_item ?>" class="hide detail_<?= $no ?>">
                 <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input id="sel_item_<?= $no ?>_<?= $no_item ?>" value="0" type="hidden"><input id="sel_item_2_<?= $no ?>_<?= $no_item ?>" value="<?= $detail['id'] ?>" type="hidden">Additional Price</td>
                 <td>Additional Price (PPN, DISC, SHIPPING COST)<input id="desc_item_<?= $no ?>_<?= $no_item ?>" value="Additional Price (PPN, DISC, SHIPPING COST)" type="hidden"></td>
@@ -37,8 +37,8 @@ $no_item = 1; ?>
                 <td>-</td>
                 <td>-</td>
                 <td><?= print_number($detail['additional_price'], 2) ?></td>
-                <td><?= print_number($detail['additional_price'] - $detail['additional_price_remaining'], 2) ?></td>
-                <td><input id="sis_item_<?= $no ?>_<?= $no_item ?>" value="<?= $detail['additional_price_remaining'] ?>" type="hidden"><?= print_number($detail['additional_price_remaining'], 2) ?></td>
+                <td><?= print_number($detail['additional_price'] - $detail['additional_price_remaining_request'], 2) ?></td>
+                <td><input id="sis_item_<?= $no ?>_<?= $no_item ?>" value="<?= $detail['additional_price_remaining_request'] ?>" type="hidden"><?= print_number($detail['additional_price_remaining_request'], 2) ?></td>
                 <td><input id="in_item_<?= $no ?>_<?= $no_item ?>" data-parent="<?= $no ?>" data-row="<?= $no_item ?>" type="number" class="sel_applied_item sel_applied_<?= $no ?>" value="0"></td>
                 <td></td>
                 <?php $no_item++; ?>
