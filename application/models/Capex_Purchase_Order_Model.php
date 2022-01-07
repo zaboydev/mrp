bff22ae7ce6032cb6f3c1467592b07252f4e4c3c
aidanurul99 <aidanurul99@rocketmail.com>
2022-01-07 16:09:24 +0800

    perbaikan get att poe di capex&expense po

--- application/models/Capex_Purchase_Order_Model.php
+++ application/models/Capex_Purchase_Order_Model.php
@@ -1514,6 +1514,7 @@ class Capex_Purchase_Order_Model extends MY_Model
     $poe_id = $id;
 
     $this->db->where('id_poe', $poe_id);
+    $this->db->where('tipe', 'POE');
     $this->db->where(array('deleted_at' => NULL));
     return $this->db->get('tb_attachment_poe')->result();
   }
