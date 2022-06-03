234f001e63b164e0f30155324a28cf469b78f8d8
nurulaida <aidanurul99@rocketmail.com>
2022-06-03 09:29:43 +0700

    perbaikan PO Capex

--- application/controllers/Capex_Purchase_Order.php
+++ application/controllers/Capex_Purchase_Order.php
@@ -790,7 +790,7 @@ class Capex_Purchase_Order extends MY_Controller
     // if ($category !== NULL){
     $item       = $this->model->findItemPoe($vendor_id);
     $order      = $this->model->findPoe($vendor_id);
-    $category   = urldecode('SPARE PART');
+    $category   = urldecode('CAPEX');
 
     $deliver    = getDefaultDeliveryTo();
     $deliver_company    = $deliver['warehouse'];
