46f5b4ce8f7703cf4853296b3c62dc25521a7c29
nurulaida <aidanurul99@rocketmail.com>
2022-12-07 12:33:10 +0700

    aircraft component planning edit

---
+++ application/config/modules/aircraft_component_plan.php
@@ -0,0 +1,20 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+$config['module']['aircraft_component_plan']['visible']         = TRUE;
+$config['module']['aircraft_component_plan']['main_warehouse']  = TRUE;
+$config['module']['aircraft_component_plan']['parent']          = 'aircraft';
+$config['module']['aircraft_component_plan']['label']           = 'Aircraft Component Plan';
+$config['module']['aircraft_component_plan']['name']            = 'aircraft_component_plan';
+$config['module']['aircraft_component_plan']['route']           = 'aircraft_component_plan';
+$config['module']['aircraft_component_plan']['view']            = config_item('module_path') .'pesawat/component_plan';
+$config['module']['aircraft_component_plan']['language']        = 'kurs_lang';
+$config['module']['aircraft_component_plan']['table']           = 'tb_master_pesawat';
+$config['module']['aircraft_component_plan']['model']           = 'Aircraft_Component_Plan_Model';
+$config['module']['aircraft_component_plan']['permission']      = array(
+    'create'  => 'PPC,MECHANIC,ADMIN,SUPER ADMIN,ENGINEER',
+    'edit'    => 'PPC,MECHANIC,ADMIN,ENGINEER,SUPER ADMIN',
+    'info'    => 'PPC,MECHANIC,ADMIN,ENGINEER,SUPER ADMIN',
+    'save'    => 'PPC,MECHANIC,ADMIN,ENGINEER,SUPER ADMIN',
+    'delete'  => 'PPC,MECHANIC,ADMIN,ENGINEER,SUPER ADMIN',
+    'index'  	=> 'PPC,MECHANIC,ADMIN,ENGINEER,SUPER ADMIN',
+);
---
+++ themes/material/modules/pesawat/component_plan/edit.php
@@ -0,0 +1,139 @@
+<?= form_open(site_url($module['route'] . '/save'), array(
+  'autocomplete'  => 'off',
+  'id'            => 'form-create-data',
+  'class'         => 'form form-validate form-xhr ui-front',
+  'role'          => 'form'
+)); ?>
+
+<div class="card style-default-bright">
+    <div class="card-head style-primary-dark">
+        <header>Edit <?= $module['label']; ?></header>
+
+        <div class="tools">
+            <div class="btn-group">
+                <a class="btn btn-icon-toggle btn-close" data-dismiss="modal" aria-label="Close" title="close">
+                <i class="md md-close"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="card-body">
+        <div class="row">
+            <div class="col-sm-12 col-lg-8">
+                <div class="form-group" style="padding-top: 20px;">
+                    <select name="aircraft_register" id="aircraft_register" data-tag-name="aircraft_register" class="form-control input-sm select2" style="width: 100%" required>
+                        <option value="">-- SELECT Aircraft --</option>
+                        <?php foreach (pesawat() as $pesawat) : ?>
+                        <option value="<?= $pesawat; ?>" <?= ($entity['aircraft_register'] == $pesawat) ? 'selected' : ''; ?>><?= $pesawat; ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                    <label for="aircraft_register">Aircraft</label>
+                </div> 
+                
+                <div class="form-group">
+                    <input type="date" name="date" id="date" class="form-control" value="<?= $entity['date']?>">
+                    <label for="date">Date</label>
+                </div>
+
+                <div class="form-group">
+                    <input type="text" name="description" id="description" class="form-control" value="<?= $entity['description']?>" required>
+                    <label for="description">Description</label>
+                </div>
+
+                <div class="form-group">
+                    <input type="text" name="part_number" id="part_number" class="form-control input-sm input-autocomplete" data-source="<?= site_url($module['route'] . '/search_items_by_part_number/'); ?>" value="<?= $entity['part_number']?>" required>
+                    <label for="part_number">Part Number</label>
+                </div>
+
+                <div class="form-group">
+                    <input type="text" name="alternate_part_number" id="alternate_part_number" class="form-control" value="<?= $entity['alternate_part_number']?>">
+                    <label for="alternate_part_number">Alternate Part Number</label>
+                </div>
+
+                <div class="form-group">
+                    <select name="unit" id="unit" class="form-control" required>
+                    <?php foreach (available_units() as $unit) : ?>
+                        <option value="<?= $unit; ?>" <?= ($entity['unit'] == $unit) ? 'selected' : ''; ?>>
+                        <?= $unit; ?>
+                        </option>
+                    <?php endforeach; ?>
+                    </select>
+                    <label for="unit">Unit of Measurement</label>
+                </div>
+
+                <div class="form-group">
+                    <select name="group" id="group" class="form-control" required>
+                    <?php foreach (available_item_groups(config_item('auth_inventory')) as $i => $group) : ?>
+                        <option value="<?= $group; ?>"<?= ($entity['group'] == $group) ? 'selected' : ''; ?>>
+                        <?= $group; ?>
+                        </option>
+                    <?php endforeach; ?>
+                    </select>
+                    <label for="group">Group</label>
+                </div>
+            </div>
+
+            <div class="col-sm-12 col-lg-4">
+                <div class="form-group">
+                    <input type="number" name="planing_quantity" id="planing_quantity" class="form-control" value="<?= $entity['planing_quantity']?>">
+                    <label for="planing_quantity">Planning Quantity</label>
+                </div>
+
+                <div class="form-group">
+                    <textarea name="remarks" id="remarks" class="form-control" rows="4"><?= $entity['remarks']?></textarea>
+                    <label for="remarks">Remarks</label>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card-foot">
+        <button type="submit" id="modal-create-data-submit" class="btn btn-floating-action btn-primary btn-xhr-submit ink-reaction pull-right" data-title="save and create">
+        <i class="md md-save"></i>
+        </button>
+    </div>
+</div>
+
+<?= form_close(); ?>
+
+<script>
+    $(document).ready(function () {
+        $.ajax({
+                url: $('input[id="part_number"]').data('source'),
+                dataType: "json",
+                success: function(resource) {
+                    $('input[id="part_number"]').autocomplete({
+                        autoFocus: true,
+                        minLength: 1,
+
+                        source: function(request, response) {
+                            var results = $.ui.autocomplete.filter(resource, request.term);
+                            response(results.slice(0, 5));
+                        },
+
+                        focus: function(event, ui) {
+                            return false;
+                        },
+
+                        select: function(event, ui) {
+                            $('input[id="part_number"]').val(ui.item.part_number);
+                            $('input[id="alternate_part_number"]').val(ui.item.alternate_part_number);
+                            $('input[id="description"]').val(ui.item.description);
+                            $('select[id="group"]').val(ui.item.group).trigger('change');
+                            $('input[id="unit"]').val(ui.item.unit);
+
+                            return false;
+                        }
+                    })
+                    .data("ui-autocomplete")._renderItem = function(ul, item) {
+                        $(ul).addClass('list divider-full-bleed');
+
+                        return $("<li class='tile'>")
+                        .append('<a class="tile-content ink-reaction"><div class="tile-text">' + item.label + '</div></a>')
+                        .appendTo(ul);
+                    };
+                }
+            });
+    });
+</script>
