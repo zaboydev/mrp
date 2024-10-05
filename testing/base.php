a1a1af14b5f3deaca6f0f9351ec8766ff923b6ec
Zakario Lesrahmanto <andrio.zaboy@gmail.com>
2024-10-05 17:53:36 +0700

    some fix

---
+++ testing/base.php
@@ -0,0 +1,50 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require_once APPPATH .'third_party/phpti/ti.php';
+
+define('LINK_PROTOCOL', USE_SSL ? 'https' : NULL);
+define('PAGE_ROLE', $this->uri->segment(2, 'index'));
+
+if (isset($page['title']))
+  define('PAGE_TITLE', strtoupper($page['title']));
+else
+  if ($module['name'] === 'dashboard')
+    define('PAGE_TITLE', 'DASHBOARD');
+  else
+    define('PAGE_TITLE', strtoupper(ucfirst( PAGE_ROLE ) .' '. $module['label']));
+
+if (isset($page['title_1']))
+  define('PAGE_TITLE_1', strtoupper($page['title_1']));
+else
+  if ($module['name'] === 'dashboard')
+    define('PAGE_TITLE_1', 'DASHBOARD');
+  else
+    define('PAGE_TITLE_1', strtoupper(ucfirst( PAGE_ROLE ) .' '. $module['label']));
+
+if (isset($page['title_2']))
+    define('PAGE_TITLE_2', strtoupper($page['title_2']));
+else
+  if ($module['name'] === 'dashboard')
+    define('PAGE_TITLE_2', 'DASHBOARD');
+  else
+    define('PAGE_TITLE_2', strtoupper(ucfirst( PAGE_ROLE ) .' '. $module['label']));
+
+$this->load->helper('html');
+
+echo html_open();
+echo html_title(PAGE_TITLE);
+echo html_charset();
+echo meta('X-UA-Compatible', 'IE=edge', 'equiv');
+echo meta('viewport', 'width=device-width, initial-scale=1.0');
+
+echo link_tag('themes/admin_lte/assets/images/favicon-plane.ico', 'shortcut icon', 'image/x-icon');
+echo link_tag('themes/admin_lte/assets/images/favicon-plane.ico', 'icon', 'image/x-icon');
+
+
+emptyblock('styles');
+emptyblock('body');
+emptyblock('scripts');
+
+echo html_close();
+?>
